874ded555b2bc46a606d0dd37779386440b626ad
Raúl Montón Pinillos <raul_m_p@me.com>
2024-08-05 17:07:40 +0200

    Initial DocC version

---
+++ Sources/SwiftZen/SwiftZen.docc/SwiftZen.md
@@ -0,0 +1,3 @@
+# ``SwiftZen``
+
+A blog about Swift.
---
+++ SwiftZen/Sources/SwiftZen/EmptyFile.swift
@@ -0,0 +1,10 @@
+//
+//  EmptyFile.swift
+//
+//
+//  Created by Raúl Montón Pinillos on 5/8/24.
+//
+
+import Foundation
+
+// This is an empty file so we can still build the documentation.
